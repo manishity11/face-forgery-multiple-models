d1fcce6dda74097381c7ed963aea8bbe066b3855
manishity11 <160490232+manishity11@users.noreply.github.com>
2024-08-08 12:07:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,13 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 import os
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input as mobilenet_preprocess_input
-from tensorflow.keras.applications.convnext import ConvNeXtBase, preprocess_input as convnext_preprocess_input
 from tensorflow.keras.applications.densenet import DenseNet121, preprocess_input as densenet_preprocess_input
 
 # Load the trained models
 mobilenet_model_path = 'best_model_net.keras'
-convnext_model_path = 'best_model_vit.keras'
 densenet_model_path = 'best_model_densenet121.keras'
 
 mobilenet_model = tf.keras.models.load_model(mobilenet_model_path)
-convnext_model = tf.keras.models.load_model(convnext_model_path)
 densenet_model = tf.keras.models.load_model(densenet_model_path)
 
 st.title("Deepfake Detection with MobileNetV2, ConvNeXt, and DenseNet121")
@@ -42,10 +39,6 @@ if uploaded_file is not None:
     mobilenet_label = "Fake" if mobilenet_prediction[0][0] > 0.5 else "Real"
     mobilenet_confidence = mobilenet_prediction[0][0]
 
-    # Preprocess and predict using ConvNeXt
-    convnext_prediction = preprocess_and_predict(convnext_model, uploaded_file, convnext_preprocess_input)
-    convnext_label = "Fake" if convnext_prediction[0][0] > 0.5 else "Real"
-    convnext_confidence = convnext_prediction[0][0]
 
     # Preprocess and predict using DenseNet121
     densenet_prediction = preprocess_and_predict(densenet_model, uploaded_file, densenet_preprocess_input)
@@ -54,13 +47,11 @@ if uploaded_file is not None:
 
     # Display the results
     st.write(f"**MobileNetV2 Prediction:** {mobilenet_label} (Confidence: {mobilenet_confidence:.4f})")
-    st.write(f"**ConvNeXt Prediction:** {convnext_label} (Confidence: {convnext_confidence:.4f})")
     st.write(f"**DenseNet121 Prediction:** {densenet_label} (Confidence: {densenet_confidence:.4f})")
 
     # Count the number of Real and Fake predictions
     class_counts = {"Real": 0, "Fake": 0}
     class_counts[mobilenet_label] += 1
-    class_counts[convnext_label] += 1
     class_counts[densenet_label] += 1
 
     st.write(f"**Class Counts:** {class_counts}")
